0e247671f47a75000612a8540791136c9bcca789
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-08-13 11:18:24 +0500

    add new future

--- src/Components/SingIn_SingUp/Components/SingUp/index.jsx
+++ src/Components/SingIn_SingUp/Components/SingUp/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 // import { SIGN_up } from "./api";
 import { gql, useMutation } from "@apollo/client";
 const SIGN_up = gql`
-  mutation SignUp($userName: String!, $password: String!) {
+  mutation signUp($userName: String!, $password: String!) {
     signUp(name: $userName, password: $password) {
       _id
       name
@@ -13,21 +13,28 @@ const SIGN_up = gql`
 
 function SingIN() {
   const [signUp, { data, loading, error }] = useMutation(SIGN_up);
-  console.log(data);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const { username, password } = e.target;
-    signUp({ userName: username, password });
+    const userName = e.target?.userName?.value;
+    const password = e.target?.password?.value;
+
+    if (userName && password) {
+      await signUp({ variables: { userName, password } });
+    }
+
+    console.log(data);
   };
+
   return (
     <div className="signup">
       <form onSubmit={handleSubmit}>
         <label for="chk" aria-hidden="true">
           Sign up
+          {loading ? "loading..." : ""}
         </label>
-        <input name="username" type="text" placeholder="First Name" required />
+        <input name="userName" type="text" placeholder="First Name" required />
         <input
           name="password"
           type="password"
--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
 
 const client = new ApolloClient({
   uri: "http://localhost:9000",
   cache: new InMemoryCache(),
 });
+
+loadDevMessages();
+loadErrorMessages();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <ApolloProvider client={client}>
       <App />
-    </React.StrictMode>
-  </ApolloProvider>
+    </ApolloProvider>
+  </React.StrictMode>
 );
